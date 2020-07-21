5b9f035defc2d4d7e65554e7ed5236d818958505
unknown <wjdthgmlgo@korea.ac.kr>
2020-07-21 17:21:29 +0900

    make: login page skeleton

--- find_thanku/src/view/login/index.js
+++ find_thanku/src/view/login/index.js
@@ -1,9 +1,47 @@
-import React from "react";
-
+import React, {useState} from "react";
+import imgsrc from "./logo2.JPG";
 import * as S from "./styles";
 
+const idbox = 'Google Account를 입력하세요'
+const pwbox = 'Password를 입력하세요'
+
 const Login = () => {
-  return <S.Login>안녕</S.Login>;
+  const [inputs, setInputs]=useState({
+    id: '',
+    pw: ''
+  });
+
+  const{id, pw} = inputs; //여기에 값이 저장됨
+
+  const onChange = (e) => { //입력시
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const onReset = () => { //버튼클릭시
+    setInputs({
+      id: '',
+      pw: ''
+    })
+  };
+
+  return <S.Login>
+    <div>
+      <img src={imgsrc} width='150px' alt='logo'/>
+    </div>
+    <div>
+      <input name='id' placeholder={idbox} onChange={onChange} value={id}/><br/>
+      <input name='pw' placeholder={pwbox} onChange={onChange} value={pw}/><br/>
+      <button onClick={onReset}>로그인</button>
+      <div>
+        값:
+        {id} ({pw})
+      </div>
+    </div>
+    </S.Login>;
 };
 
 export default Login;
