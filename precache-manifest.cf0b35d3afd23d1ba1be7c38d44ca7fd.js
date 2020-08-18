d1ca437f1daa6011e383d806d587b861781881fc
deltaori0 <deltaori0@korea.ac.kr>
2020-08-18 20:04:47 +0900

    Updates

---
+++ precache-manifest.cf0b35d3afd23d1ba1be7c38d44ca7fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb2a3b1be66d62cd507da07ea540992",
+    "url": "/KU_Lost_and_Found/index.html"
+  },
+  {
+    "revision": "de8e7c67194dddf7b185",
+    "url": "/KU_Lost_and_Found/static/css/main.cc86dba1.chunk.css"
+  },
+  {
+    "revision": "ffd293225d69d4804e48",
+    "url": "/KU_Lost_and_Found/static/js/2.5b887f05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/KU_Lost_and_Found/static/js/2.5b887f05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de8e7c67194dddf7b185",
+    "url": "/KU_Lost_and_Found/static/js/main.ac8045e5.chunk.js"
+  },
+  {
+    "revision": "d77daca42bd0ef3bf76e",
+    "url": "/KU_Lost_and_Found/static/js/runtime-main.a5986eb9.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/KU_Lost_and_Found/precache-manifest.cf0b35d3afd23d1ba1be7c38d44ca7fd.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/KU_Lost_and_Found/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
