19720a9e1fe410845d7c6b95195d6f63755dbf15
deltaori0 <deltaori0@korea.ac.kr>
2020-08-18 20:05:14 +0900

    publish page

--- find_thanku/src/view/join/index.js
+++ find_thanku/src/view/join/index.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
 import * as S from "./styles";
 import Layout from "../../component/layout";
 
 const Join = () => {
+  const [userID, setUserID] = useState("");
+
+  const handleSubmit = useCallback(async () => {
+    const searchResponse = await fetch(
+      `http://localhost:4000/api/user?id=${userID}`
+    );
+  }, [userID]);
   return (
     <Layout>
       <S.Join>
         <S.JoinContainer>
-          <S.IDInputBox />
-          <S.PWInputBox />
-          <S.PWConfirmInputBox />
-          <S.SubmitButton>확인</S.SubmitButton>
+          <S.IDInputBox placeholder="아이디" />
+          <S.PWInputBox placeholder="비밀번호" />
+          <S.PWConfirmInputBox placeholder="비밀번호 확인" />
+          <S.SubmitButton onClick={handleSubmit}>확인</S.SubmitButton>
         </S.JoinContainer>
       </S.Join>
     </Layout>
