fd6e5fa75b68514c9045d827d08c19030b3795e9
deltaori0 <deltaori0@korea.ac.kr>
2020-08-28 16:18:00 +0900

    feat: show mypage data

--- find_thanku/src/view/mypage/hooks2.js
+++ find_thanku/src/view/mypage/hooks2.js
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react";
 
 export const useMyPage2 = (googleId) => {
-  const [posts, setPosts] = useState([]);
+  const [posts2, setPosts] = useState([]);
 
   useEffect(() => {
     const getInitialPosts = async () => {
-      const boardkind = "find"
-      const request = await fetch("http://localhost:4000/mypage/"+boardkind+"/"+googleId, {
-        method: "GET",
-      });
-      console.log("http://localhost:4000/mypage/"+boardkind+"/"+googleId);
+      const boardkind = "find";
+      const request = await fetch(
+        "http://localhost:4000/mypage/" + boardkind + "/" + googleId,
+        {
+          method: "GET",
+        }
+      );
+      console.log("http://localhost:4000/mypage/" + boardkind + "/" + googleId);
       if (!request.ok) {
         alert("서버 죽음");
         return;
@@ -25,6 +28,6 @@ export const useMyPage2 = (googleId) => {
   }, []);
 
   return {
-    posts,
+    posts2,
   };
 };
--- find_thanku/src/view/mypage/styles.js
+++ find_thanku/src/view/mypage/styles.js
@@ -15,6 +15,28 @@ export const BoardListContainer = styled.div`
   height: 100%;
 `;
 
+export const MyInfoContainer = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  margin-bottom: 3rem;
+`;
+export const MyInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const MetaContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Meta = styled.div`
+  font-size: 1.4rem;
+  margin-bottom: 0.8rem;
+`;
 export const TitleContainer = styled.div`
   width: 100%;
 
@@ -24,10 +46,11 @@ export const TitleContainer = styled.div`
 `;
 
 export const Title = styled.div`
+  width: 100%;
   font-size: 2.2rem;
   font-weight: bold;
 
-  margin-bottom: 0.3rem;
+  margin-bottom: 1.8rem;
 `;
 export const Label = styled.div`
   font-size: 1.8rem;
@@ -37,16 +60,14 @@ export const Label = styled.div`
 `;
 
 export const ImageContainer = styled.div`
-img {
-  width: 8rem;
-  height: 8rem;
-  border-radius: 8rem;
-}
+  img {
+    width: 8rem;
+    height: 8rem;
+    border-radius: 8rem;
+  }
+  margin-right: 3rem;
 `;
 
-export const Temp = styled.div`
-  font-size: 1.8rem;
-  font-weight: bold;
-
-  margin-bottom: 0.3rem;
+export const Container = styled.div`
+  margin-bottom: 2rem;
 `;
