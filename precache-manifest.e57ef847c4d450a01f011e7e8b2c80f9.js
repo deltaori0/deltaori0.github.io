4c155754f9808a6f0ef3038aad2175418f71e5d3
deltaori0 <deltaori0@korea.ac.kr>
2020-08-30 16:39:36 +0900

    Updates

---
+++ precache-manifest.e57ef847c4d450a01f011e7e8b2c80f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1cd8b79a090e6a9a3edb78bc3aee12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b10a0457d3ec1fbf08d",
+    "url": "/static/css/main.176858f2.chunk.css"
+  },
+  {
+    "revision": "cf7ad269acef72ff0fc9",
+    "url": "/static/js/2.ce610a65.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ce610a65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b10a0457d3ec1fbf08d",
+    "url": "/static/js/main.27520b0a.chunk.js"
+  },
+  {
+    "revision": "124fb8ffab9a92a4cf49",
+    "url": "/static/js/runtime-main.fea93d6e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/KU_Lost_and_Found/precache-manifest.bb11f62df49367a3d0aeb6b3a7f3f959.js"
+  "/precache-manifest.e57ef847c4d450a01f011e7e8b2c80f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/KU_Lost_and_Found/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
